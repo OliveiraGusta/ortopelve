8f99d18ddbcafb17496a0370f55d557bc4d931ad
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-08-15 23:25:53 -0300

    <FEAT>[Login Page]: Começo da tela de login

--- index.php
+++ index.php
@@ -2,15 +2,23 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="vierport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ortopelve</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/login.ss">
+    <link rel="stylesheet" href="css/login.css">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+
 </head>
 <body>
-    
+    <div class="login-form">
+        <form action="" method="post">
+            <div class="logo">
+                <img src="img\logo-quadrado.png">
+            </div>
+        </form>     
 </body>
 </html>
