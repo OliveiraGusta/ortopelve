3a67dfeea8ecde6718b5dea298c74c99610d1fed
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-08-17 02:55:42 -0300

    <CHORE>[Home Page]: Mudança de cores e diagramacao

--- painel-adm/index.php
+++ painel-adm/index.php
@@ -20,7 +20,7 @@
     <img  class="float-start" src="../img/logo-horizontal.png">
     <li class="float-end nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Administrador - Gustavo Oliveira
+            Administrador - Gustavo Oliveira
           </a>
           <div class="dropdown-menu">
             <a class="dropdown-item" href="../index.php">Sair</a>
@@ -29,10 +29,53 @@
 </div>
 </nav>
 
-    <div class="text-center">
-        <h2>Pagína em Construção</h2>
+<div class="container-fluid">
+<div class="d-flex align-items-start">
+  <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
+        Home
+    </button>
+    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">
+    Controle de Pacientes
+    </button>
+    
+    <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">
+    Controle Financeiro
+    </button>
+    <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">
+    Configurações
+    </button>
+    <button class="nav-link" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled" type="button" role="tab" aria-controls="v-pills-disabled" aria-selected="false" disabled>
+    Area Reserva
+    </button>
+</div>
+    <div class="tab-content" id="v-pills-tabContent">
+    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+        Voltar para Home
+        </div>
+        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
+        Pacientes
     </div>
+    
+    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="0">
+    Controle Financeiro
+    </div>
+    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">
+    Configurações
+    </div>
+    <div class="tab-pane fade" id="v-pills-disabled" role="tabpanel" aria-labelledby="v-pills-disabled-tab" tabindex="0">
+    Area Extra
+    </div>
+</div>
+  
 
+
+</div>
+
+
+
+
+    
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
