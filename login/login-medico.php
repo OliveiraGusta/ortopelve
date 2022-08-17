d84df74fd1385c101a4ebe9ebf4978c038dba16b
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-08-17 00:56:10 -0300

    <CHORE>[Login Page]: Opcao de escolha entre medico e paciente

--- login/login-medico.php
+++ login/login-medico.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,41 +9,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS Boostrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/home.css">
-    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="img/favicon/favicon.ico" type="image/x-icon">  
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="shortcut icon" href="../img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../img/favicon/favicon.ico" type="image/x-icon">  
     
 </head>
 <body>
     <div class="login-form">
         <form action="" method="post">
             <div class="logo">
-                <img src="img\logo-quadrado.png" alt="Logo Ortopelve">
+                <img src="..\img\logo-quadrado.png" alt="Logo Ortopelve">
             </div>
             <h2 class="text-center"> 
-            OrtoPelve Fisioterapia</h2>
+            Médico / Fisioterapeuta</h2>
             <h3 class="text-center"> 
             Entre no Sistema</h3>
 
-            <div class="form-group d-grid gap-2">
-             <button class="btn btn-primary" type="submit" name="btn-login"> 
-                <a class="btn-escolha" href="./login/login-medico.php">Médico / Fisioterapeuta</a></button>
+            <div class=" ">
+                <input class="form-control" type="email" name="usuario" 
+                placeholder="E-mail" required> 
+            </div>
+
+            <div class="form-group">
+                <input class="form-control" type="password" name="senha" 
+                placeholder="Senha" required> 
             </div>
 
             <div class="form-group d-grid gap-2">
-             <button class="btn btn-primary" type="submit" name="btn-login"> 
-                <a class="btn-escolha" href="./login/login-paciente.php">Paciente</a></button>
+                <button class="btn btn-primary" type="submit" name="btn-login"> 
+                    Entrar </button>
             </div>
 
             <div class="clearfix">
-            <a href="#" class="float-start">Sou novo por aqui</a>
-            
-            <a href="" class="float-end">Voltar...</a>            
+                <label class="float-start checkbox-inline">
+                <input type="checkbox">
+                Lembrar-me
+            </label>
+            <a href="../index.php" class="meio-voltar">Voltar</a>
+
+            <a href="#" class="float-end">Recuperar Senha</a>
             </div>
 
         </form> 
+
+
+
 
 <!-- JavaScript Boostrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>    
 </body>
 </html>
+
