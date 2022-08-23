87d4aa4481b33d0ddb68112d9d9e47ef5f59333a
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-08-23 01:03:51 -0300

    <CHORE>[Home Page]: Designer Card da Home e Notificacoes

--- painel-adm/index.php
+++ painel-adm/index.php
@@ -1,5 +1,10 @@
+<?php
+$notificacoes = 3;
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <title>Painel Administrativo</title>
 
@@ -8,74 +13,72 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS Boostrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/painel.css">
     <link rel="shortcut icon" href="../img/favicon/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="../img/favicon/favicon.ico" type="image/x-icon">  
-    
+    <link rel="icon" href="../img/favicon/favicon.ico" type="image/x-icon">
+
 </head>
+
 <body>
-<nav class="navbar bg-light">
-<div class="col-md-12">
-    <img  class="float-start" src="../img/logo-horizontal.png">
-    <li class="float-end nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Administrador - Gustavo Oliveira
-          </a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="../index.php">Sair</a>
-          </div>
-        </li>
-</div>
-</nav>
-
-<div class="container-fluid">
-<div class="d-flex align-items-start">
-  <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
-        Home
-    </button>
-    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">
-    Controle de Pacientes
-    </button>
-    
-    <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">
-    Controle Financeiro
-    </button>
-    <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">
-    Configurações
-    </button>
-    <button class="nav-link" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled" type="button" role="tab" aria-controls="v-pills-disabled" aria-selected="false" disabled>
-    Area Reserva
-    </button>
-</div>
-    <div class="tab-content" id="v-pills-tabContent">
-    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
-        Voltar para Home
+    <nav class="navbar bg-light">
+        <div class="col-md-12 mt-3">
+            <img class="float-start" src="../img/logo-horizontal.png">
+            <li class="float-end nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Administrador - Gustavo Oliveira
+                </a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="../index.php">Sair</a>
+                </div>
+            </li>
         </div>
-        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
-        Pacientes
+    </nav>
+
+
+    <div class="container-fluid mt-4">
+        <div class="d-flex align-items-start mb-4">
+            <div class="nav flex-column nav-pills me-5" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <div class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home"
+                    type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</div>
+                <div class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile"
+                    type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Lista de Paciente
+                </div>
+                <div class="nav-link" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled"
+                    type="button" role="tab" aria-controls="v-pills-disabled" aria-selected="false" disabled>Controle
+                    Financeiro</div>
+                <?php if($notificacoes > 0) {?>
+                <div class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages"
+                    type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">
+                    Mensagens <span class="badge text-bg-dark"> <?php echo  $notificacoes;?>
+                        </span>
+                </div>
+                <?php } ?>
+
+            </div>
+
+            <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel"
+                    aria-labelledby="v-pills-home-tab" tabindex="0"> <?php 
+            include_once("home.php");
+            ?></div>
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"
+                    tabindex="0">...</div>
+                <div class="tab-pane fade" id="v-pills-disabled" role="tabpanel" aria-labelledby="v-pills-disabled-tab"
+                    tabindex="0">...</div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"
+                    tabindex="0">Notificacoes</div>
+
+
+            </div>
+        </div>
     </div>
-    
-    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="0">
-    Controle Financeiro
-    </div>
-    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">
-    Configurações
-    </div>
-    <div class="tab-pane fade" id="v-pills-disabled" role="tabpanel" aria-labelledby="v-pills-disabled-tab" tabindex="0">
-    Area Extra
-    </div>
-</div>
-  
 
 
-</div>
-
-
-
-
-    
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
 </body>
